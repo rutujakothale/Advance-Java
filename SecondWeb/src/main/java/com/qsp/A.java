b74b961810abcea0e3993fb7f0b61046c96dddef
Rutuja Kothale <rutujashokothale2602@gmail.com>
2026-01-21 23:00:31 +0530

    Add files via upload

---
+++ SecondWeb/src/main/java/com/qsp/A.java
@@ -0,0 +1,49 @@
+package com.qsp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class A  implements Servlet{
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+			System.out.println("A servlet class");
+			
+			PrintWriter p= res.getWriter();
+			p.write("<html><body>");
+			p.write("<h1>Servlet class A<h1>");
+			p.write("</html></body>");
+			//p.print(); - can use this method also
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
